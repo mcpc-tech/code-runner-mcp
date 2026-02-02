be2eb3c33e9bc4ef4e0a80c6a5c55ee9e0b25450
yao <63141491+yaonyan@users.noreply.github.com>
2026-02-02 11:20:42 +0800

    ci: add GitHub workflow for JSR and npm publishing

--- src/stdio.server.ts
+++ src/stdio.server.ts
@@ -2,4 +2,7 @@ import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
 import { server } from "./app.ts";
 
 const transport = new StdioServerTransport();
-await server.connect(transport);
+
+(async () => {
+  await server.connect(transport);
+})();
